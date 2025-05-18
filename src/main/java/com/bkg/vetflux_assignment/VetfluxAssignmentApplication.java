4fd2591472462f74d5e6e60a66ac4d817e5d0ca0
BKGod <bongkyo.god@gmail.com>
2025-05-18 16:24:48 +0900

    spring boot init

---
+++ src/main/java/com/bkg/vetflux_assignment/VetfluxAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.bkg.vetflux_assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VetfluxAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VetfluxAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bkg/vetflux_assignment/VetfluxAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bkg.vetflux_assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VetfluxAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
